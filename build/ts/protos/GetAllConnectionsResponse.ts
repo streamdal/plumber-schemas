69f996befb51f799a4bb77a818ffedaf599f0c47
Mark Gregan <mark@batch.sh>
2021-07-22 13:37:59 -0400

    GetAllConnectionsResponse now returns array/slice

--- build/ts/protos/GetAllConnectionsResponse.ts
+++ build/ts/protos/GetAllConnectionsResponse.ts
@@ -3,9 +3,9 @@
 import type { Connection as _protos_Connection, Connection__Output as _protos_Connection__Output } from '../protos/Connection';
 
 export interface GetAllConnectionsResponse {
-  'connections'?: ({[key: string]: _protos_Connection});
+  'connections'?: (_protos_Connection)[];
 }
 
 export interface GetAllConnectionsResponse__Output {
-  'connections': ({[key: string]: _protos_Connection__Output});
+  'connections': (_protos_Connection__Output)[];
 }
